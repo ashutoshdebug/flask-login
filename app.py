a72b400d2bbd98b4c54f429984cb3e4d9ed415f3
ashutoshdebug <ashutoshkumart82@gmail.com>
2025-10-02 09:26:47 +0530

    Phase 1 completed

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, redirect, url_for, session, Response
 
 app = Flask(__name__)
+app.secret_key = "supersecret" # It requires as we are dealing with the login system, it locks the session for a user so that only the desired user can access the session
 
 #homepage login page
 
@@ -17,5 +18,26 @@ def login():
             return Response("Invalid credentials. Try again", mimetype="text/plain") #mimetype helps to tell the app to send data in which form, by default it sends it in html type, text/plain tells it to send in plain form
     
     return """
-
+<h2>Login Page</h2>
+<form method = "POST">
+Username: <input type = "text" name = "username"><br />
+Password: <input type = "text" name = "password"><br />
+<input type = "submit" value = "login">
+</form>
 """
+
+# WELCOME page (after login)
+@app.route("/welcome")
+def welcome():
+    if "user" in session:
+        return f'''
+        <h2> Welcome, {session["user"]}!</h2>
+        <a href = {url_for('logout')}>Logout</a>
+'''
+    return redirect(url_for("login"))
+
+# logout route
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
